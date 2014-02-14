e0a3adae60bb2e935cc5b766781b757e2477df55
pawel <pawel.niechoda@gmail.com>
2014-02-14 23:18:23 +0200

    adds scaffolding for filtering by game

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,16 @@
 /**
  * Created by pawel on 1/25/14.
  */
-app.controller('Videos', function($scope) {
+app.controller('Videos', function($scope, Everyplay) {
 		//handler for videos
 
+		Everyplay.getGames()
+			.then(function(games) {
+				$scope.games = games;
+			})
+		$scope.filterByGame = function(game) {
+			alert(game.name);
+		}
 	})
 	.controller('Video', function($scope) {
 		//handler for specific video page
--- js/services.js
+++ js/services.js
@@ -1,3 +1,20 @@
 /**
  * Created by pawel on 2/14/14.
  */
+app.factory('Everyplay', function($q) {
+		return {
+			getGames: function() {
+				var deferred = $q.defer();
+				// todo add fetching games with api, for now returns static games data
+				var games = [{
+					id: 10,
+					name: 'Game 1'
+				},{
+					id: 20,
+					name: 'Game 2'
+				}]
+				deferred.resolve(games);
+				return deferred.promise;
+			}
+		}
+	})
